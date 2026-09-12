174b789975056ac7865f806367f2a38335ded232
Jni3w <ikun26206@gmail.com>
2026-09-12 06:38:52 -0300

    Add files via upload

--- content.js
+++ content.js
@@ -1,18 +1,15 @@
 let preV=null;
 let createTimeoutID=null;
 let clearTimeoutID=null;
+const save=new Map();
+const maxV=10;
+initialize();
 
 document.addEventListener("mouseover",async (e) => {
-    createTimeoutID=await setTimeout(()=>{
-        const link=e.target.closest("a");
-        if(link&&link.href.includes("huijiwiki.com")){
-            getInfo(link);
-        }
-    },500);
-    if(preV){
-        preV.addEventListener("mouseover",(e)=>{
-            clearTimeout(clearTimeoutID);
-        })
+    const link=e.target.closest("a");
+    if(link&&link.href.includes("huijiwiki.com")){
+        clearTimeout(clearTimeoutID);
+        getInfo(link);
     }
 })
 
@@ -21,20 +18,22 @@ document.addEventListener("mouseout",(e)=>{
         return;
     }
     clearTimeout(createTimeoutID);
-    if(e.relatedTarget){
-        if(e.relatedTarget.id=="preV"){
-            preV.addEventListener("mouseout",(e)=>{
-                if(e.target.tagName!=="a"){
-                    hide();
-                }
-            })
-        }
-        else{
-            hide();
-        }
-    }
-
+    hide();
 })
+
+preV.addEventListener("mouseover",()=>{
+    if(preV.style.display=="block"){
+        console.log("enter");
+        clearTimeout(clearTimeoutID);
+    }
+})
+
+preV.addEventListener("mouseout",(e)=>{
+    if(e.relatedTarget&&e.relatedTarget.tagName!="A"){
+        hide();
+    }
+})
+
 
 async function getInfo(link){
     const url=new URL(link.href);
@@ -48,11 +47,21 @@ async function getInfo(link){
         format:"json",
     });
     try{
+        if(save.has(link.href)){
+            const temp=save.get(link.href);
+            display(temp[0],temp[1]);
+            console.log("activated");
+            return;
+        }
         const response=await fetch(searchApi);
         const content=await response.json();
         const page=Object.values(content.query.pages)[0];
         console.log(page);
         const rect=link.getBoundingClientRect();
+        if(save&&save.size==maxV){
+            save.delete(save.keys().next().value);
+        }
+        save.set(link.href,[page,rect]);
         display(page,rect);
     }
     catch(error){
@@ -61,24 +70,26 @@ async function getInfo(link){
 }
 
 function display(page,rect){
-    if(!preV){
-        preV=document.createElement("div");
-        preV.id="preV";
-        document.body.appendChild(preV);
-    }
-    preV.style.display="block";
-    preV.innerHTML=page.extract;
-    preV.style.top=rect.bottom+"px";
-    preV.style.left=rect.left+"px";
-    console.log(preV.style.position)
-    console.log("created");
+    createTimeoutID=setTimeout(()=>{
+        preV.style.display="block";
+        preV.innerHTML=page.extract;
+        preV.style.top=rect.bottom+"px";
+        preV.style.left=rect.left+"px";
+        console.log("displayed");
+    },500)
 }
 
 function hide(){
-    if(preV){
-        clearTimeoutID=setTimeout(()=>{
-            preV.style.display="none";
-            console.log("erased");
-        },300)
-    }
+    console.log("hide");
+    clearTimeoutID=setTimeout(()=>{
+        preV.style.display="none";
+        console.log("erased");
+    },500)
+}
+
+function initialize(){
+    preV=document.createElement("div");
+    preV.id="preV";
+    preV.style.display="none";
+    document.body.appendChild(preV);
 }
